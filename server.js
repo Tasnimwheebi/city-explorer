dcf16d5edd1f6d7c390f38990d30cda9f32f3cda
Tasnimwheebi <tasnimwheebi@gmail.com>
2021-04-11 17:45:25 +0300

    fix the movies part

--- server.js
+++ server.js
@@ -20,10 +20,10 @@ const PORT = process.env.PORT || 3000;
 
 const client = new pg.Client( process.env.DATABASE_URL ); //for local testing
 
-// const client = new pg.Client ( {
-//   connectionString:process.env.DATABASE_URL,
-//   ssl:{rejectUnauthorized:false}
-// } );
+const client = new pg.Client ( {
+  connectionString:process.env.DATABASE_URL,
+  ssl:{rejectUnauthorized:false}
+} );
 
 const superagent = require( 'superagent' );
 
@@ -142,6 +142,34 @@ function Park( data ) {
   this.fee = data.entranceFees[0].cost;
   this.description = data.description;
   this.url = data.url;
+
+}
+
+
+server.get( '/movies',( req,res )=>{
+  let cityName = req.query.search_query;
+  let keyMovie = process.env.MOVIE_API_KEY;
+  let movieUrl = `https://api.themoviedb.org/3/search/movie?api_key=${keyMovie}&query=${cityName}`;
+  superagent.get( movieUrl )
+    .then( movieData => {
+      let mData = movieData.body.results;
+      let movieARR = [];
+      mData.map( dataM => {
+        movieARR.push( new Movie ( dataM ) );
+
+      } );
+      res.send( movieARR );
+    } );
+} );
+
+function Movie ( data ) {
+  this.title = data.original_title;
+  this.overview = data.overview;
+  this.average_votes = data.average_votes;
+  this.total_votes = data.total_votes;
+  this.image_url = `https://image.tmdb.org/t/p/w500${data.poster_path}`;
+  this.popularity = data.popularity;
+  this.released_on = data.released_on;
 
 }
 
